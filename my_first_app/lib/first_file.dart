7628d4ad5e9b9c3db7123983513742c4e5481c18
shaheeralics <shaheeralics@hu.edu.pk>
2024-03-12 09:58:35 +0500

    my_first_app

---
+++ my_first_app/lib/first_file.dart
@@ -0,0 +1,12 @@
+void main() {
+  List student = ["Shaheer_Ali", 301, 221, 044];
+  int age_1 = 65;
+  String name = "653244643532";
+  double gpa_2 = 3.5;
+  var kuchbhi = 'Khuraya';
+  print(gpa_2);
+  print(student);
+  print(name);
+  print(age_1);
+  print(kuchbhi);
+}
